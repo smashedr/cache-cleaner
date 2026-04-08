919b1b6107796508023e3c9e873f04f4fa1c7871
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-04-07 17:27:18 -0700

    Updates (#2)

--- src/entrypoints/sidepanel/main.ts
+++ src/entrypoints/sidepanel/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import '@/main.ts'
 import './style.css'
 import { bootstrapDirective } from '@/directives/bootstrap.ts'
+import App from './App.vue'
 
 createApp(App).directive('bs', bootstrapDirective).mount('#app')
