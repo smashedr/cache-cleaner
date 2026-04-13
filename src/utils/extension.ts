4d434cf6a4c269f40e2b2c36080dc972bea6deae
Shane <6071159+smashedr@users.noreply.github.com>
2026-04-12 18:02:29 -0700

    Updates

--- src/utils/extension.ts
+++ src/utils/extension.ts
@@ -68,13 +68,13 @@ export async function openExtPanel(close = false) {
       const panel = await chrome.windows.get(lastPanelID)
       // console.debug('panel', panel)
       console.debug('panel?.id', panel?.id)
-      if (panel) {
+      if (panel?.id) {
         const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
         // console.debug('tabs:', tabs)
         console.debug('tabs[0]?.windowId:', tabs[0]?.windowId)
         if (panel.id != tabs[0]?.windowId) {
           console.debug('%c Panel found:', 'color: SpringGreen', panel.id)
-          await chrome.windows.update(lastPanelID, { focused: true })
+          await chrome.windows.update(panel.id, { focused: true })
           if (close) window.close()
           return
         }
